af4f40a63f5f3098c488c52111d48ffa55c34ed5
elsagis <elsagis@yahoo.com>
2023-01-23 21:55:22 -0600

    Commit

--- process_streaming_ElsaGhirmazion.py
+++ process_streaming_ElsaGhirmazion.py
@@ -18,18 +18,19 @@ socket_type = socket.SOCK_DGRAM
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
 # read from a file to get some fake data
-input_file = open("proces_streaming_ElsaGhirmazion.csv", "r")
+input_file = open("process_streaming_elsaghirmazion.csv", "r")
+output_file = open("out9.txt", 'w')
+# To get them in chronological order use the builtin sorted() function
+reversed = sorted(input_file)
 
 # create a csv reader for our comma delimited data
-reader = csv.reader(input_file, delimiter=",")
+reader = csv.reader(reversed, delimiter=",")
 
-output_file_name = "out9.txt"
-output_file = open(output_file_name, "w", newline='')
 
 writer = csv.writer(output_file, delimiter=",")
 
 header = next(reader)
-header_list = ['Id', 'IndicatorCode', 'SpatialDim', 'SpatialValueCode', 'TimeDim/year', 'Value', 'NumericValue', 'Date', 'TimeDimensionValue', 'TimeDimensionBegin', 'TimeDimensionEnd']
+header_list = ['Id', 'IndicatorCode', 'SpatialDim', 'SpatialValueCode', 'TimeDim_year', 'Value', 'NumericValue', 'Date', 'TimeDimensionValue', 'TimeDimensionBegin', 'TimeDimensionEnd']
 
 writer.writerow(header_list)
 
@@ -38,19 +39,20 @@ for row in reader:
     Id, IndicatorCode, SpatialDim, SpatialValueCode, TimeDimyear, Value, NumericValue, Date, TimeDimensionValue, TimeDimensionBegin, TimeDimensionEnd = row 
 
     # use an fstring to create a message from our data
-    fstring_message = f"[{Id}, {IndicatorCode}, {SpatialDim}, {SpatialValueCode}, {TimeDim/year}, {value}, {NumericValue}, {Date}, {TimeDimensionValue}, {TimeDimensionBegin}, {TimeDimensionEnd}]"
+    fstring_message = f"[{Id}, {IndicatorCode}, {SpatialDim}, {SpatialValueCode}, {TimeDim_year}, {value}, {NumericValue}, {Date}, {TimeDimensionValue}, {TimeDimensionBegin}, {TimeDimensionEnd}]"
     
     # prepare a binary (1s and 0s) message to stream
+    output_file.write(fstring_message)
     MESSAGE = fstring_message.encode()
 
     # use the socket sendto() method to send the message
     sock.sendto(MESSAGE, ADRESS_TUPLE)
-    print (f"Sent: {MESSAGE} on port {port}.")
-    writer.writerow([Id, IndicatorCode, SpatialDim, SpatialValueCode, TimeDim/year, Value, NumericValue, Date, TimeDimensionValue, TimeDimensionBegin, TimeDimensionEnd])
+    print (f"Sent: {MESSAGE} on port {port},\n")
+    writer.writerow([Id, IndicatorCode, SpatialDim, SpatialValueCode, TimeDim_year, Value, NumericValue, Date, TimeDimensionValue, TimeDimensionBegin, TimeDimensionEnd])
 
 
     # sleep for a few seconds
-    time.sleep(3)
+    time.sleep(1)
 output_file.close()
 input_file.close()
 
